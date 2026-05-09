d694d99977182ce67736911cb059f5770a6a21ba
bothost-source <destinyjob493@gmail.com>
2026-05-09 21:15:34 +0100

    Create exif.js

---
+++ library/exif.js
@@ -0,0 +1,8 @@
+module.exports = {
+    videoToWebp: async () => {},
+    writeExifImg: async () => {},
+    writeExifVid: async () => {},
+    addExif: async () => {},
+    toPTT: async () => {},
+    toAudio: async () => {}
+};
